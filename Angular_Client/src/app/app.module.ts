8bdfb04cf5cb85a6e4d010bf362479e0df8ab2ad
prottoy <prottoyroyaiub@gmail.com>
2021-08-15 22:17:51 +0600

    clientrevised

--- Angular_Client/src/app/app.module.ts
+++ Angular_Client/src/app/app.module.ts
@@ -20,10 +20,10 @@ import { NotFoundComponent } from './error-pages/not-found/not-found.component';
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
-      { path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) },
+      { path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
       { path: '404', component : NotFoundComponent},
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: '**', redirectTo: '/404', pathMatch: 'full'}
+      // { path: '', redirectTo: '/home', pathMatch: 'full' },
+      // { path: '**', redirectTo: '/404', pathMatch: 'full'}
     ])
   ],
   providers: [],
